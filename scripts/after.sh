59e77ae0304be7694d9b1b335907b236195bd71e
Akshil <akshilshah4444@gmail.com>
2026-01-11 14:34:53 +0530

    misc

--- scripts/after.sh
+++ scripts/after.sh
@@ -2,11 +2,14 @@ cd /home/ubuntu/backtested
 
 # download the env file
 current_env=`cat /etc/backtested/env`
-sudo -u ubuntu /home/ubuntu/.local/bin/aws s3 cp s3://ak-apps-configs/$current_env/$current_env.backtested.env .env
+sudo -u ubuntu /usr/local/bin/aws s3 cp s3://ak-apps-configs/$current_env/$current_env.backtested.env .env
 
 # link the shared node_modules
 ln -s /home/ubuntu/shared/backtested/node_modules node_modules
 
+# Run yarn commands as ubuntu user with full environment
+sudo -u ubuntu -i bash << 'EOF'
+cd /home/ubuntu/backtested
 yarn
 echo "Applying migrations"
 
@@ -24,3 +27,4 @@ if [ "$checker" -ne "0" ]; then
 fi
 
 yarn build
+EOF
