8b0fbaad1d284e9184fd7f308d3e63ecc68a9c41
Akshil <akshilshah4444@gmail.com>
2026-01-11 14:40:26 +0530

    misc

--- scripts/after.sh
+++ scripts/after.sh
@@ -7,23 +7,23 @@ sudo -u ubuntu /usr/local/bin/aws s3 cp s3://ak-apps-configs/$current_env/$curre
 
 ln -s /home/ubuntu/shared/backtested/node_modules node_modules
 
-# Set PATH to include common locations for node and yarn
-export PATH="/usr/local/bin:/usr/bin:/bin:$PATH"
+# Set PATH to include NVM node/yarn location
+YARN_PATH="/home/ubuntu/.nvm/versions/node/v22.18.0/bin"
 
 # Run yarn commands as ubuntu user
-sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"/usr/local/bin:/usr/bin:/bin:\$PATH\" && yarn"
+sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"$YARN_PATH:\$PATH\" && yarn"
 echo "Applying migrations"
 
-sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"/usr/local/bin:/usr/bin:/bin:\$PATH\" && yarn db:migrate:apply"
+sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"$YARN_PATH:\$PATH\" && yarn db:migrate:apply"
 checker=$?
 if [ "$checker" -ne "0" ]; then
     exit $checker
 fi
 
-sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"/usr/local/bin:/usr/bin:/bin:\$PATH\" && yarn db:generate"
+sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"$YARN_PATH:\$PATH\" && yarn db:generate"
 checker=$?
 if [ "$checker" -ne "0" ]; then
     exit $checker
 fi
 
-sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"/usr/local/bin:/usr/bin:/bin:\$PATH\" && yarn build"
+sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"$YARN_PATH:\$PATH\" && yarn build"
