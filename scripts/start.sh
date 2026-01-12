114c6b31808199089e321a8e843fece1a4fbb4cf
Akshil <akshilshah4444@gmail.com>
2026-01-12 10:52:47 +0530

    misc

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 cd /home/ubuntu/backtested
 
-yarn stop
+# Set PATH to include NVM node/yarn location
+YARN_PATH="/home/ubuntu/.nvm/versions/node/v22.18.0/bin"
 
-yarn start
+# Run yarn commands as ubuntu user
+sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"$YARN_PATH:\$PATH\" && yarn stop"
+
+sudo -u ubuntu bash -c "cd /home/ubuntu/backtested && export PATH=\"$YARN_PATH:\$PATH\" && yarn start"
